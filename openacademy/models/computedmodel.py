7d1d94c83a67fa3f4fb792e520c8550069e9073c
anibal <anibalmauriciod@gmail.com>
2021-02-15 18:04:34 +0000

    [NEW]DefaultValues

---
+++ openacademy/models/computedmodel.py
@@ -0,0 +1,11 @@
+import random
+from odoo import models, fields, api
+
+class ComputedModel(models.Model):
+    _name = 'test.computed'
+
+    name = fields.Char(compute='_compute_name')
+
+    def _compute_name(self):
+        for record in self:
+            record.name = str(random.randint(1, 1e6))
